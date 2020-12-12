b7c50c8c4464232106979bc6d9ea81e1089fa396
wep1980 <wepbike@gmail.com>
2020-12-12 07:57:45 -0300

    Primeiro commit

---
+++ backend/src/main/java/br/com/waldirep/sistemapesquisa/SistemapesquisaApplication.java
@@ -0,0 +1,13 @@
+package br.com.waldirep.sistemapesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemapesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemapesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/waldirep/sistemapesquisa/SistemapesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.waldirep.sistemapesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemapesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

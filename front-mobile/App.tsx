fc2998032d64ecc84c5d044cde0fd1884d7d7895
wep1980 <wepbike@gmail.com>
2020-12-15 15:12:52 -0300

    Finalizando App React Native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,8 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { AppLoading } from 'expo';
-import { useFonts, Play_400Regular, Play_700Bold } from '@expo-google-fonts/play';
+import {AppLoading} from 'expo';
+import {
+  useFonts,
+  Play_400Regular,
+  Play_700Bold
+} from '@expo-google-fonts/play';
+
+
 import Routes from './src/routes';
 
 export default function App() {
@@ -11,21 +17,20 @@ export default function App() {
     Play_700Bold
   });
 
-  if(!fontsLoaded){ // Se as fontes não estiverem carregadas vai aparecer o load...se o inicia a aplicação depois das fontes estarem ok
-    return <AppLoading/>
+  if (!fontsLoaded) {
+    return <AppLoading />
   } else {
     return (
-      <View style={styles.container}>
+        <View style={styles.container}>
         <Routes />
         <StatusBar style="light" />
       </View>
     );
   }
- 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  },
+  }
 });
